b6b5a7361385907607b3cd769bcce240ac25faa1
Mohammad Omar Salah Hariri <mosh@proactive.dk>
2020-02-06 20:48:23 +0100

    1.1 tasks in the book

---
+++ tasks/1.1.ts
@@ -0,0 +1,13 @@
+/**
+ * 1- Find the maximum of the array value
+ * 2- Compute the Average of the array value
+ * 3- Copy to another array
+ * 4- Reverse the element within an array
+ * 5- Matrix-matrix multiplication ( square matrices )
+ * 6- Absolute value of an a int
+ * 7- Absolute value of a double value
+ * 8- primality test
+ * 9- Square root
+ * 10- Hypotenus of a right triangle
+ * 11- Harmonic number
+ */
